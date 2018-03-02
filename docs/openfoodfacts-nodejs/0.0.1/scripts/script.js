f2f77eb71fc80229e2f0f5e35dfbe5554bfc77a9
Mohit Sharma <msvbs98@gmail.com>
2018-03-02 13:58:03 +0530

    feat(bla bla): thisi is awesome
    jai mata di

--- docs/openfoodfacts-nodejs/0.0.1/scripts/script.js
+++ docs/openfoodfacts-nodejs/0.0.1/scripts/script.js
@@ -1,43 +1,41 @@
-/*global document, prettyPrint */
+/* global document, prettyPrint */
 
-(function() {
-
+(function () {
   var source,
-      i = 0,
-      lineNumber = 0,
-      lineId,
-      lines,
-      totalLines,
-      anchorHash,
-      navTrigger = document.querySelector("#nav-trigger");
+    i = 0,
+    lineNumber = 0,
+    lineId,
+    lines,
+    totalLines,
+    anchorHash,
+    navTrigger = document.querySelector('#nav-trigger')
 
-  prettyPrint();
+  prettyPrint()
 
-  source = document.getElementsByClassName("prettyprint source linenums");
+  source = document.getElementsByClassName('prettyprint source linenums')
 
   if (source && source[0]) {
-    anchorHash = document.location.hash.substring(1);
-    lines = source[0].getElementsByTagName("li");
-    totalLines = lines.length;
+    anchorHash = document.location.hash.substring(1)
+    lines = source[0].getElementsByTagName('li')
+    totalLines = lines.length
 
     for (; i < totalLines; i++) {
-      lineNumber++;
-      lineId = "line" + lineNumber;
-      lines[i].id = lineId;
+      lineNumber++
+      lineId = 'line' + lineNumber
+      lines[i].id = lineId
 
       if (lineId === anchorHash) {
-        lines[i].className += " selected";
+        lines[i].className += ' selected'
       }
     }
   }
 
-  //Closes mobile nav on item click
-  document.querySelectorAll("nav ul > li > a").forEach(function (el) {
+  // Closes mobile nav on item click
+  document.querySelectorAll('nav ul > li > a').forEach(function (el) {
     el.onclick = function (evt) {
       if (navTrigger.checked) {
-        navTrigger.checked = false;
+        navTrigger.checked = false
       }
-    };
-  });
-
-})();
+    }
+  })
+})()
